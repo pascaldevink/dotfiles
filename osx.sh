abcb0dd01ff4c8d92885c46ae1597e7b2f76f920
Pascal de Vink <pascal.de.vink@gmail.com>
2015-01-25 20:58:16 +0100

    Increased the size of the dock

--- osx.sh
+++ osx.sh
@@ -540,7 +540,7 @@ running "Enable highlight hover effect for the grid view of a stack (Dock)"
 defaults write com.apple.dock mouse-over-hilite-stack -bool true;ok
 
 running "Set the icon size of Dock items to 36 pixels"
-defaults write com.apple.dock tilesize -int 36;ok
+defaults write com.apple.dock tilesize -int 50;ok
 
 running "Change minimize/maximize window effect to scale"
 defaults write com.apple.dock mineffect -string "scale";ok
